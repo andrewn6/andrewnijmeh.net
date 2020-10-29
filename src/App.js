27bfe24b7a794cf1021d80262485bbc884b235e4
andrew <andrewnijmeh1@gmail.com>
2020-10-29 13:10:09 -0400

    import navbar and proper import syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container'
-
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 
 
