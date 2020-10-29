6c356a8c1a76e53cfe5560427db979f65e0dfcc4
andrew <andrewnijmeh1@gmail.com>
2020-10-29 10:02:33 -0400

    import router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Component from 'react';
+import Router from 'react';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
 // Local importd (assets, css, html etc)
@@ -61,7 +63,8 @@ class App extends Component {
           <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
           <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
           <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
-          <Route path="/projects" render{() => <ProjectsPage title={this.state.projects.title} />} />
+          <Route path="/contact" render={() => <ProjectsPage title={this.state.projects.title} />} />
+          
           
           <Footer />
 
