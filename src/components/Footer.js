8f2e9b6c8940ae697ca77c419c3a17f7026cbf01
andrew <andrewnijmeh1@gmail.com>
2020-11-04 21:28:23 -0500

    finissh creating footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,3 +2,23 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/row';
 import Col from 'react-bootstrap/Col';
+
+function Footer(){
+    return(
+        <footer className="mt-5">
+            <Container fluid={true}>
+                <Row className="border-top justify-content-between p3">
+                    <Col className="p=0" md={3} sm={12}>
+                        Andrew Nijmeh
+                    </Col>
+                    <Col classname="p-0 d-flex justify-content-end" md={3}>
+                    This site was made by Andrew Nijmeh.
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+            
+            
+        
+    );
+}
