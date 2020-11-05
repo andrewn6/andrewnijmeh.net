9d50d30888d283763c98854698a16bf413781d37
andrew <andrewnijmeh1@gmail.com>
2020-11-04 21:45:34 -0500

    format

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/row';
 import Col from 'react-bootstrap/Col';
 
-function Footer(){
+function Footer() {
     return(
         <footer className="mt-5">
             <Container fluid={true}>
