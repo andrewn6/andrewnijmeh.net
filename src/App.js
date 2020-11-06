688777166c9bd3d0c64561335a86d5b5fca0dd7d
andrew <andrewnijmeh1@gmail.com>
2020-11-05 21:21:45 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ class App extends Component {
           <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
           <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
           <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
-          <Route path="/projects render={() => <ProjectsPage title={this.state.projects.title} />} />
+          <Route path="/projects" render={() => <ProjectsPage title={this.state.projects.title} />} />
           
           
           <Footer />
