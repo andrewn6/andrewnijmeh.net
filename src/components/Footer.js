44a3d2d730f6644971c19e85ba9c42974883b1bb
andrew <andrewnijmeh1@gmail.com>
2020-11-05 19:36:10 -0500

    export defualt function

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/row';
 import Col from 'react-bootstrap/Col';
 
-function Footer() {
+export default function Footer() {
     return(
         <footer className="mt-5">
             <Container fluid={true}>
@@ -23,4 +23,3 @@ function Footer() {
     );
 }
 
-export default Footer;
