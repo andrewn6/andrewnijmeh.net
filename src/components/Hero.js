798bd092cab2bfb7e9892fa08f47dc6a7e432919
andrew <andrewnijmeh1@gmail.com>
2020-11-05 19:18:57 -0500

    add props.text for content in hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,6 +13,7 @@ function Hero(props) {
                     <Col md={8} sm={12}>
                     { props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1> }
                     { props.subTitle && <h3 className="display-4 font-weight-light">{props.subTitle}</h3> }
+                    { props.text && <h3 className="lead font-weight-light">{props.text}</h3> }
                     </Col>
                 </Row>
             </Container>
