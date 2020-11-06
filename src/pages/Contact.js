3866b5beee79821981ac778703b51d8d4a7a7daa
andrew <andrewnijmeh1@gmail.com>
2020-11-05 21:09:19 -0500

    remove crap

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Component } from 'react';
 
 import Footer from '../components/Footer'
-import Button from 'react-bootstrap/Button';
+
 
 class Contact extends React.Component {
     constructor(props){
