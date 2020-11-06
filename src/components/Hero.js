c2a2e06d1aba7782da1fb221a5ffca720fa2d9cb
andrew <andrewnijmeh1@gmail.com>
2020-11-05 19:18:08 -0500

    add props subtitle

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,7 +11,8 @@ function Hero(props) {
             <Container fluid={true}>
                 <Row className="justify-content-center py-5">
                     <Col md={8} sm={12}>
-                        
+                    { props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1> }
+                    { props.subTitle && <h3 className="display-4 font-weight-light">{props.subTitle}</h3> }
                     </Col>
                 </Row>
             </Container>
