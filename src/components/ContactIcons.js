82e227b0c9fbfc4750f23383e37e46e190282682
andrew <andrewnijmeh1@gmail.com>
2020-11-05 19:43:55 -0500

    yes add youtube icon

--- src/components/ContactIcons.js
+++ src/components/ContactIcons.js
@@ -7,6 +7,17 @@ import {
 } from "@Fontawesome/free-brands-svg-icons"
 
 export default function ContactIcons() {
+    return(
+        <div className="contact-icons">
+            <h3>My socials!</h3>
+            <a
+                href="https://www.youtube.com/c/Flop"
+                className="youtube-icon"
+            >
+                <FontAwesomeIcon icon={faYoutube} size="2x" />
+            </a>
+        </div>
 
+    )
 }
 
