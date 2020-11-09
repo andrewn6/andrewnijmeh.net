8e7d0df9a1efb608ca194845aa7205364e7330cd
andrew <andrewnijmeh1@gmail.com>
2020-11-09 15:48:02 -0500

    add more

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,6 +13,7 @@ class AboutPage extends Component {
                 <Hero title={props.title} />
                 <Content>
                     <p>Hello, my name is Andrew Nijmeh. I'm a backend engineer/developer with experience with many framework tools and technologies.</p>
+                    <p>I know many frameworks and tools such as, Node.js, React.js, Python, Flask, Express, Postgres, Docker, Docker-Compose, Git Github and lastly CI/CD</p>
                 </Content>
             </div>
         );
