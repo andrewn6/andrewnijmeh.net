5e4c730aadcaa66e904730ff9896d25325d44969
andrew <andrewnijmeh1@gmail.com>
2020-11-10 18:21:02 -0500

    rename class

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Component } from 'react';
 import Content from '../components/Content'
 
-class Skills extends Component {
+class SkillsPage extends Component {
     constructor(props){
         super(props);
         this.state = {
