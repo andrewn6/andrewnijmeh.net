b7e791b0d66449d4f9e295eb4e2bbcb032e94fa2
andrew <andrewnijmeh1@gmail.com>
2020-11-10 18:07:25 -0500

    create skills class extend component

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,3 +1,18 @@
 import React from 'react';
 import { Component } from 'react';
 import Content from '../components/Content'
+
+export default class Skills extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            title: "Skills",
+            
+            frameworks: {
+                title: "Frameworks",
+                subtitle: "Here are a list"
+            },
+
+        }
+    }
+}
