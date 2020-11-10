859544a3ab1a4e6f76f6696e1a96079ce8ed9088
andrew <andrewnijmeh1@gmail.com>
2020-11-10 18:40:30 -0500

    test

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,8 @@ class AboutPage extends Component {
     
     render(){
         return(
-            <div>
+            <div className="about">
+                <div className="about-content">
                 <Hero title={props.title} />
                 <Content>
                     <p>Hello, my name is Andrew Nijmeh. I'm a backend engineer/developer with experience with many framework tools and technologies.</p>
@@ -17,6 +18,7 @@ class AboutPage extends Component {
                     <p>I'm currently working on a few projects, utility bot a large scale discord bot with many features. I'm also working on a quiz app with Aditya Kapoor (TheCodingGuru)</p>
                     <p>My dream one day is to be a open source maintainer and a project leader.</p>
                 </Content>
+                </div>
             </div>
         );
     }
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Component } from 'react';
 import Content from '../components/Content'
 
+
 class SkillsPage extends Component {
     constructor(props){
         super(props);
