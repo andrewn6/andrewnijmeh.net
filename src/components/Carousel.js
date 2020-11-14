a7e2108ce8ade20ae580b488dfc94e783c6ffdcb
andrew <andrewnijmeh1@gmail.com>
2020-11-14 12:10:30 -0500

    rename to jsx

--- src/components/Carousel.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-class Casrouel extends Component{
-
-}
-
-export default Carousel;
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+class Carousel extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return(
+
+        );
+    }
+
+}
+
+export default Carousel;
