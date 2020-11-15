5f998c65fc37d214d0896090ca1dd62cc546b87f
andrew <andrewnijmeh1@gmail.com>
2020-11-14 19:29:39 -0500

    app.listen()

--- api/index.ts
+++ api/index.ts
@@ -2,4 +2,6 @@ import express from 'express';
 
 const app = express();
 
-const port  = process.env.PORT || 8080
+const port  = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`Listening on PORT ${port}`));
