919d7dd229dca099edd042e06cb560a343252578
andrew <andrewnijmeh1@gmail.com>
2020-11-14 22:18:28 -0500

    import link

--- src/Errors/404.jsx
+++ src/Errors/404.jsx
@@ -1,7 +1,7 @@
-import { render } from '@testing-library/react';
 import React from 'react';
 import { Component } from 'react';
 import PageNotFound from '../assets/images/404.png';
+import { Link } from 'react-router';
 
 class Error404 extends Component{
 
