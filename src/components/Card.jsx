da14d84bcda0718d2620fbd19deaedd65c1a0c5b
andrew <andrewnijmeh1@gmail.com>
2020-11-15 10:13:59 -0500

    my script needed to be in car.djs

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Cardinfo from './components/Cardinfo';
+
+function Card(props) {
+    return(
+        <div className="d-inline-block g-card" onClick={(e) => props.click(props.item)}>
+            <img className="g-card-image" src={props.item.imgSrc} alt={props.item.imgSrc} />
+            { props.item.selected && <Cardinfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link} /> }
+        </div>
+    )
+}
