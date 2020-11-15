6492d2821940a6d4d8f67b11b0e519db4475b97a
andrew <andrewnijmeh1@gmail.com>
2020-11-15 10:40:22 -0500

    finish prop for utilitybot

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 
+import Card from '../components/Card';
 
-export default class Carousel extends Component{
+import utilitybot from '../assets/images/utilitybot.png';
 
+
+class Carousel extends Component{
     constructor(props){
         super(props);
         this.state = {
+            items: [
+                {
+                    id: 0,
+                    title: 'Utility Bot',
+                    subTitle: 'A discord bot with many integerations features and moderation.',
+                    imgSrc: utilitybot,
+                    link: 'https://utilitybot.co',
+                    selected: false
 
+                }
+            ]
         }
     }
-
-
-    render() {
-        return(
-
-        );
-    }
-
 }
