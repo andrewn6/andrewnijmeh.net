b1734ba53bb5bbc1ccaa0e843556c3fb0cea14d3
andrew <andrewnijmeh1@gmail.com>
2020-11-15 10:21:45 -0500

    finish cardinfo

--- src/components/Cardinfo.jsx
+++ src/components/Cardinfo.jsx
@@ -1,1 +1,14 @@
-import Re
+import React from 'react';
+import { useSpring, animated } from 'react-spring';
+
+function CardInfo(props) {
+    const style = useSpring({opacity: 1, from: {opacity: 0}});
+
+    return(
+        <animated.div class="g-card-info style" style={style}>
+            <p className="g-card-title">{props.title}</p>
+            <p className="g-card-sub-title">{props.subTitle}</p>
+            <a href={props.link} target="_blank" rel="noopener noreferrer">View</a>
+        </animated.div>
+    );
+}
