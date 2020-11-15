125885a6d7b8ed5777efa33660ae7af0a7b97359
andrew <andrewnijmeh1@gmail.com>
2020-11-14 22:06:05 -0500

    create /errors for 404 etc

---
+++ src/components/Errors/404.jsx
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import { Component } from 'react';
+
+class 404 extends Component{
+
+    render(){
+        <div>
+            <img src={PageNotFound} />
+            <p style={{textAlign: "center"}}>
+                <Link to"/
+
+        </div>
+    }
+}
+
+export default 404page;
