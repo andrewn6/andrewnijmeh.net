6724a21172cd0474a89bf7680332882bbab121d4
andrew <andrewnijmeh1@gmail.com>
2020-11-17 16:22:07 -0500

    classes: create projcet-subtitle and project-title

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -4,9 +4,16 @@ import Carousel from './components/Carousel';
 import css from './src/css/projects.css';
 
 class ProjectsPage extends Component{
-    
+
     render(){
         return(
+            <div>
+                <div className="project-title">
+                    <div className="project-subtitle">
+                        <h3>Here are a list of my projects with a link to the github repository.</h3>
+                    </div>
+                </div>  
+            </div>
 
         );
     }
