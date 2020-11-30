fb5451a4bebe2d61d214ad3bc292d73728b55488
andrew <andrewnijmeh1@gmail.com>
2020-11-29 20:17:37 -0500

    fix last dirs

--- src/pages/SkillsPage.jsx
+++ src/pages/SkillsPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Component } from 'react';
-import Content from '../components/Content'
+import Content from '/components/Content/Content'
 
 import css from './src/css/skills.css'
 
